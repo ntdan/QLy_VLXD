379706792d3617321ec826fa62c33d9c0f6ada9a
Ngô Tường Dân <ngotuongdan@gmail.com>
2017-05-28 16:19:07 +0700

    update

--- app/src/main/java/com/cuahang/qly_vlxd/Customer_UnCompletedPayment.java
+++ app/src/main/java/com/cuahang/qly_vlxd/Customer_UnCompletedPayment.java
@@ -6,8 +6,17 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.ListView;
+
+import com.cuahang.qly_vlxd.invoice.Invoice;
 
 public class Customer_UnCompletedPayment extends AppCompatActivity {
+
+    Invoice invoice;
+    ListView listViewInv;
+    boolean chonMua = false;
+    Invoice.DBAdapter adapter;
+    private int invCode = 1000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +33,19 @@ public class Customer_UnCompletedPayment extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
+        if (invoice == null) {
+            listViewInv = (ListView) findViewById(R.id.lvInvoice);
+            invoice = new Invoice(Customer_UnCompletedPayment.this);
+            invoice.setId(0);
+            invoice.find();
+            if (invoice.list != null && invoice.list.size() > 0) {
+                adapter = invoice.new DBAdapter(invoice.list, Customer_UnCompletedPayment.this);
+                listViewInv.setAdapter(adapter);
+            }
+
+            registerForContextMenu(listViewInv);
+        }
     }
 
 }
